a2a876e84dd07dbdaaba4528e400715390d4bc74
Igor Gritsenko <ctocopokcemb147@yandex.ru>
2023-06-04 13:54:50 +0300

    popup fixed, add alt to image, page min-heght fixed

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,49 @@
 // Блок с попапом для редактирования профиля
 
 const editPopup = document.querySelector('.popup_profile-edit');
-const OpenPopupButton = document.querySelector('.profile__edit-button');
-const ClosePopupButton = document.querySelector('.popup__close-button_profile');
+const openPopupButton = document.querySelector('.profile__edit-button');
+const closePopupButton = document.querySelector('.popup__close-button_profile');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 const inputTitle = document.querySelector('.popup__input_profile_name');
 const inputSubtitle = document.querySelector('.popup__input_profile_job');
 const editForm = document.querySelector('.popup__form_profile');
 
-function openPopup() {
-  editPopup.classList.add('popup_opened');
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+}
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+// function openPopup() {
+//   editPopup.classList.add('popup_opened');
+//   inputTitle.value = profileTitle.textContent;
+//   inputSubtitle.value = profileSubtitle.textContent;
+// }
+
+// closePopup(editPopup);
+
+// function closePopup() {
+//   editPopup.classList.remove('popup_opened');
+// }
+
+openPopupButton.addEventListener('click', () => {
+  openPopup(editPopup);
   inputTitle.value = profileTitle.textContent;
   inputSubtitle.value = profileSubtitle.textContent;
-}
-
-function closePopup() {
-  editPopup.classList.remove('popup_opened');
-}
-
-OpenPopupButton.addEventListener('click', openPopup);
-ClosePopupButton.addEventListener('click', closePopup);
+});
+closePopupButton.addEventListener('click', () => {
+  closePopup(editPopup);
+});
 editForm.addEventListener('submit', function (event) {
   event.preventDefault();
 
   profileTitle.textContent = inputTitle.value;
   profileSubtitle.textContent = inputSubtitle.value;
 
-  closePopup();
+  closePopup(editPopup);
 });
 
 // Попап с картинкой
@@ -35,17 +51,19 @@ editForm.addEventListener('submit', function (event) {
 const popupImage = document.querySelector('.popup_image');
 const popupImageOpened = popupImage.querySelector('.popup__image-opened');
 const popupImageText = popupImage.querySelector('.popup__image-text');
-const popupImageDeleteButton = popupImage.querySelector('.popup__close-button_image');
+const popupImageCloseButton = popupImage.querySelector('.popup__close-button_image');
 
-function openPopupImage() {
-  popupImage.classList.add('popup_opened');
-}
+// function openPopupImage() {
+//   popupImage.classList.add('popup_opened');
+// }
 
-function closePopupImage() {
-  popupImage.classList.remove('popup_opened');
-}
+// function closePopupImage() {
+//   popupImage.classList.remove('popup_opened');
+// }
 
-popupImageDeleteButton.addEventListener('click', closePopupImage);
+popupImageCloseButton.addEventListener('click', () => {
+  closePopup(popupImage);
+});
 
 // Добваления карточек на страницу через массив элементов
 
@@ -53,33 +71,6 @@ const template = document.querySelector('.elements__item-template');
 const templateContent = template.content;
 const elementsItem = templateContent.querySelector('.elements__item');
 const elementsItems = document.querySelector('.elements__items');
-
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
 
 initialCards.forEach(function (card) {
   const cardElement = createCard(card);
@@ -93,6 +84,7 @@ function createCard(card) {
 
   cardTitle.textContent = card.name;
   cardLink.src = card.link;
+  cardLink.alt = card.name;
 
   const deleteButton = cardElement.querySelector('.elements__delete-button');
   deleteButton.addEventListener('click', function (event) {
@@ -104,11 +96,12 @@ function createCard(card) {
     likeButton.classList.toggle('elements__like-button_active');
   });
 
-  cardLink.addEventListener('click', function () {
+  cardLink.addEventListener('click', () => {
     popupImageOpened.src = cardLink.src;
+    popupImageOpened.alt = cardLink.alt;
     popupImageText.textContent = card.name;
 
-    openPopupImage();
+    openPopup(popupImage);
   });
 
   return cardElement;
@@ -124,16 +117,20 @@ const editFormAdd = document.querySelector('.popup__form_card');
 // const inputSubtitle = editPopup.querySelector('.popup__input_profile_job');
 // console.log(inputTitle);
 
-function openPopupAdd() {
-  editPopupAdd.classList.add('popup_opened');
-}
+// function openPopupAdd() {
+//   editPopupAdd.classList.add('popup_opened');
+// }
 
-function closePopupAdd() {
-  editPopupAdd.classList.remove('popup_opened');
-}
+// function closePopupAdd() {
+//   editPopupAdd.classList.remove('popup_opened');
+// }
 
-openPopupAddButton.addEventListener('click', openPopupAdd);
-closePopupAddButton.addEventListener('click', closePopupAdd);
+openPopupAddButton.addEventListener('click', () => {
+  openPopup(editPopupAdd);
+});
+closePopupAddButton.addEventListener('click', () => {
+  closePopup(editPopupAdd);
+});
 editFormAdd.addEventListener('submit', function (event) {
   event.preventDefault();
 
@@ -144,5 +141,5 @@ editFormAdd.addEventListener('submit', function (event) {
 
   elementsItems.prepend(cardElement);
   formCard.reset();
-  closePopupAdd();
+  closePopup(editPopupAdd);
 });
